789a95bac0e53b4f2d57234c493bf500d2268aed
yasinsky93 <yasinsky93@gmail.com>
2021-09-11 19:40:14 +0300

    MRA: Change h1 text

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // index.js
 // создаем элемент заголовка
 const heading = document.createElement('h1')
-heading.textContent = '111js is connected to HTML and insert this H1!'
+heading.textContent = 'TS is compiled, connected to HTML and insert this H1!'
 
 // добавляем заголовок в DOM
 const root = document.querySelector('#root')
