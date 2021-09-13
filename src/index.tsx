fbe026af69e1378dc9529e7abd809a1f6e5bae73
yasinsky93 <yasinsky93@gmail.com>
2021-09-13 18:34:25 +0300

    MRA: Create Clock as class

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,46 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-interface IAppProps { // описывает содержание
-    title: string;
+interface ClockState {
+    date: Date;
 }
 
-function App (props: IAppProps) {
-    return <h1>{props.title}</h1>;
+interface Clock {
+    timerId: number;
 }
+
+class Clock extends React.Component<object, ClockState> {
+    constructor(props: object) {
+        super(props);
+        this.state = {
+            date: new Date()
+        }
+    }
+
+    componentDidMount() {
+        this.timerId = window.setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Привет, мир!</h1>
+                <h2>Сейчас {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        )
+    }
+
+    tick() {
+        this.setState({ date: new Date() });
+    }
+}
+
 
 ReactDOM.render(
-    <App title="We have just connected React to our app!" />,
+    <Clock />,
     document.getElementById('root')
 );
