6bb03f3df1e80bd667a1f0827d7a134031f43208
yasinsky93 <yasinsky93@gmail.com>
2021-09-30 09:54:07 +0300

    MRA: Add warning banner and toggle button

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,51 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Clock } from './components/clock';
+import { Toggle } from './components/toggle';
+
+class WarnngPannel extends React.Component<{}, {showWarning: Boolean}> {
+    constructor(props) {
+      super(props);
+      this.state = { showWarning: true }
+    }
+  
+    handleToggleClick() {
+      this.setState(prevState => ({
+        showWarning: !prevState.showWarning
+      }));
+    }
+    
+    render() {
+      return (
+        <div>
+          <WarningBanner warn={this.state.showWarning} />
+          <button onClick={this.handleToggleClick.bind(this)}>
+            {this.state.showWarning ? 'Hide' : 'Show'}
+          </button>
+        </div>
+      );
+    }
+  }
+  
+class WarningBanner extends React.Component<{warn: Boolean}, {}> {
+    render() {
+        if (!this.props.warn) {
+            return null;
+        }
+    
+        return (
+            <div className="warning">
+                Warning!
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-    <Clock />,
+    <>
+        <WarnngPannel />
+        <Clock />
+        <Toggle />
+    </>,
     document.getElementById('root')
 );
